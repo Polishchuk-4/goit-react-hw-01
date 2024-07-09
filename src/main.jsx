10905520ee3308f8a3bb7aa8e043b0260cddd36a
Denys Polishchuk <polischyk0000@gmail.com>
2024-07-09 14:48:18 +0300

    delete module from index && reset

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import "./components/reset.module.css";
-import "./index.module.css";
+import "./components/reset.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
